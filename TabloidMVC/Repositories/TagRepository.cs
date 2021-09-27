f387f42909b22525d6a9e6b20f06b796caac2017
kor0sensei <averyolmstead@comcast.net>
2021-09-27 15:39:00 -0500

    still have to do tag repository

--- TabloidMVC/Repositories/TagRepository.cs
+++ TabloidMVC/Repositories/TagRepository.cs
@@ -11,30 +11,5 @@ namespace TabloidMVC.Repositories
 {
     public class TagRepository : BaseRepository, ITagRepository
     {
-        public TagRepository(IConfiguration config) : base(config) { }
-        public List<Post> GetAllPublishedPosts()
-        {
-            using (var conn = Connection)
-            {
-                conn.Open();
-                using (var cmd = conn.CreateCommand())
-                {
-                    cmd.CommandText = @"
-                       SELECT Tag.Id, 
-                              Tag.Name";
-
-                    var reader = cmd.ExecuteReader();
-
-                    var posts = new List<Post>();
-
-                    while (reader.Read())
-                    {
-                        .Add(NewTagFromReader(reader));
-                    }
-
-                    reader.Close();
-
-                    return posts;
-                }
-            }
-        }
+    }
+}
