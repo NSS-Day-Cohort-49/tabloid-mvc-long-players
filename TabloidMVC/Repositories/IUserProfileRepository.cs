7cd9d3aa511c4366b63428a2c4a140c9752b2fe0
Thomas Witherow <twitherow@bellsouth.net>
2021-09-28 13:20:35 -0500

    added user profile views

--- TabloidMVC/Repositories/IUserProfileRepository.cs
+++ TabloidMVC/Repositories/IUserProfileRepository.cs
@@ -8,10 +8,10 @@ namespace TabloidMVC.Repositories
 
         List<UserProfile> GetAll();
         UserProfile GetByEmail(string email);
-        //UserProfile GetUserProfileById(int Id);
+        UserProfile GetUserProfileById(int Id);
         //void Add(UserProfile userProfile);
         //void Update(UserProfile userProfile);
-        //void Delet(UserProfile userProfile);
+        //void Delete(UserProfile userProfile);
 
     }
 }
--- TabloidMVC/Repositories/UserProfileRepository.cs
+++ TabloidMVC/Repositories/UserProfileRepository.cs
@@ -42,6 +42,10 @@ namespace TabloidMVC.Repositories
                             ImageLocation = DbUtils.GetNullableString(reader, "ImageLocation"),
                             UserTypeId = reader.GetInt32(reader.GetOrdinal("UserTypeId")),
                             UserType = new UserType()
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("UserTypeId")),
+                                Name = reader.GetString(reader.GetOrdinal("UserTypeName"))
+                            },
                         });
                     }
 
@@ -94,6 +98,56 @@ namespace TabloidMVC.Repositories
                     reader.Close();
 
                     return userProfile;
+                }
+            }
+        }
+
+        public UserProfile GetUserProfileById(int id)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                      SELECT u.id, u.FirstName, u.LastName, u.DisplayName, u.Email,
+                              u.CreateDateTime, u.ImageLocation, u.UserTypeId,
+                              uT.[Name] AS UserTypeName
+                         FROM UserProfile u
+                              LEFT JOIN UserType uT ON u.UserTypeId = uT.id
+                       WHERE u.id = @id";
+
+                    cmd.Parameters.AddWithValue("@id", id);
+                    var reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        UserProfile userProfile = new UserProfile
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Email = reader.GetString(reader.GetOrdinal("Email")),
+                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                            LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                            DisplayName = reader.GetString(reader.GetOrdinal("DisplayName")),
+                            CreateDateTime = reader.GetDateTime(reader.GetOrdinal("CreateDateTime")),
+                            ImageLocation = DbUtils.GetNullableString(reader, "ImageLocation"),
+                            UserTypeId = reader.GetInt32(reader.GetOrdinal("UserTypeId")),
+                            UserType = new UserType()
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("UserTypeId")),
+                                Name = reader.GetString(reader.GetOrdinal("UserTypeName"))
+                            },
+                        };
+
+                        reader.Close();
+                        return userProfile;
+                    }
+                    else
+                    {
+                        reader.Close();
+                        return null;
+                    }
+
                 }
             }
         }
