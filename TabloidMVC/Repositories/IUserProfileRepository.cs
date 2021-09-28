10492611f5e97b2446365fc954ca2619032cb81c
Thomas Witherow <twitherow@bellsouth.net>
2021-09-28 10:33:00 -0500

    eh

--- TabloidMVC/Repositories/IUserProfileRepository.cs
+++ TabloidMVC/Repositories/IUserProfileRepository.cs
@@ -1,9 +1,17 @@
 ﻿using TabloidMVC.Models;
+using System.Collections.Generic;
 
 namespace TabloidMVC.Repositories
 {
     public interface IUserProfileRepository
     {
+
+        //List<UserProfile> GetAll();
         UserProfile GetByEmail(string email);
+        //UserProfile GetUserProfileById(int Id);
+        //void Add(UserProfile userProfile);
+        //void Update(UserProfile userProfile);
+        //void Delet(UserProfile userProfile);
+
     }
 }
